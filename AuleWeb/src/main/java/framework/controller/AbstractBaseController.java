2e4d20135cd38a96394e4054c814de73a932a82e
Ackerman977 <emanueletraversa1997@gmail.com>
2023-12-19 16:43:06 +0100

    uuu

--- AuleWeb/src/main/java/framework/controller/AbstractBaseController.java
+++ AuleWeb/src/main/java/framework/controller/AbstractBaseController.java
@@ -17,7 +17,7 @@ import javax.naming.NamingException;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest;   
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
