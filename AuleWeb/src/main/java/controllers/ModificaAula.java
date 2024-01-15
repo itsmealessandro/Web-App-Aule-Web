07166c37a68e3a86df4b3847d5398350cfce6ece
itsmealessandro <itsmealessandro@outlook.com>
2024-01-15 20:17:38 +0100

    preparazione Modfica Aula

--- AuleWeb/src/main/java/controllers/ModificaAula.java
+++ AuleWeb/src/main/java/controllers/ModificaAula.java
@@ -33,7 +33,8 @@ public class ModificaAula extends AuleWebBaseController {
   }
 
   private void action_update(HttpServletRequest request, HttpServletResponse response, int a_key, String nome,
-      String descrizione)
+      String luogo, String edificio, String piano, int capienza, int preseElettriche, int preseRete,
+      String note, String attrezzatura)
       throws IOException, ServletException, TemplateManagerException {
 
     try {
@@ -41,9 +42,17 @@ public class ModificaAula extends AuleWebBaseController {
       AuleWebDataLayer dataLayer = (AuleWebDataLayer) request.getAttribute("datalayer");
 
       Aula aula = dataLayer.getAulaDAO().getAulaByID(a_key);
-      request.setAttribute("aula", aula);
+      aula.setNome(nome);
+      aula.setLuogo(luogo);
+      aula.setEdificio(edificio);
+      aula.setPiano(piano);
+      aula.setCapienza(capienza);
+      aula.setPreseElettriche(preseElettriche);
+      aula.setPreseRete(preseRete);
+      aula.setNote(note);
+      // TODO gestire l'attrezzatura
 
-      res.activate("adminModificaDip.ftl.html", request, response);
+      res.activate("operazioneEseguita.ftl.html", request, response);
 
     } catch (DataException ex) {
       handleError("Data access exception: " + ex.getMessage(), request, response);
@@ -57,13 +66,28 @@ public class ModificaAula extends AuleWebBaseController {
     request.setAttribute("page_title", "Modifica Dipartimento");
     try {
       if (request.getParameter("a_key") != null &&
-          request.getParameter("name") != null &&
-          request.getParameter("descr") != null) {
+          request.getParameter("nome") != null &&
+          request.getParameter("edificio") != null &&
+          request.getParameter("luogo") != null &&
+          request.getParameter("piano") != null &&
+          request.getParameter("capienza") != null &&
+          request.getParameter("preseElettriche") != null &&
+          request.getParameter("preseRete") != null &&
+          request.getParameter("attrezzatura") != null &&
+          request.getParameter("note") != null) {
 
         int a_key = SecurityHelpers.checkNumeric(request.getParameter("a_key"));
-        String nome = request.getParameter("name");
-        String descrizione = request.getParameter("descrizione");
-        action_update(request, response, a_key, nome, descrizione);
+        String nome = request.getParameter("nome");
+        String luogo = request.getParameter("luogo");
+        String edificio = request.getParameter("edificio");
+        String piano = request.getParameter("piano");
+        int capienza = SecurityHelpers.checkNumeric(request.getParameter("capienza"));
+        int preseElettriche = SecurityHelpers.checkNumeric(request.getParameter("preseElettriche"));
+        int preseRete = SecurityHelpers.checkNumeric(request.getParameter("preseRete"));
+        String note = request.getParameter("note");
+        String attrezzatura = request.getParameter("attrezzatura");
+        action_update(request, response, a_key, nome, luogo, edificio, piano, capienza, preseElettriche,
+            preseRete, note, attrezzatura);
 
       } else if (request.getParameter("a_key") != null) {
         int a_key = SecurityHelpers.checkNumeric(request.getParameter("a_key"));
