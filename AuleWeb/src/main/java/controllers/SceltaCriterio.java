b25636a3d172d9f8b9611e1fb779405d9238dd1a
itsmealessandro <itsmealessandro@outlook.com>
2024-02-20 00:33:18 +0100

    [U] preparata esportazione

--- AuleWeb/src/main/java/controllers/SceltaCriterio.java
+++ AuleWeb/src/main/java/controllers/SceltaCriterio.java
@@ -1,10 +1,4 @@
-
 package controllers;
-
-import framework.data.DataException;
-import framework.result.TemplateManagerException;
-import framework.result.TemplateResult;
-import framework.security.SecurityHelpers;
 
 import java.io.IOException;
 import java.sql.Date;
@@ -12,10 +6,15 @@ import java.sql.Date;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import data.dao.AuleWebDataLayer;
 import data.domain.Aula;
 import data.domain.Corso;
 import data.domain.Dipartimento;
+import framework.data.DataException;
+import framework.result.TemplateManagerException;
+import framework.result.TemplateResult;
+import framework.security.SecurityHelpers;
 
 public class SceltaCriterio extends AuleWebBaseController {
 
@@ -108,6 +107,22 @@ public class SceltaCriterio extends AuleWebBaseController {
     response.sendRedirect(url.toString());
   }
 
+  private void action_esporta(HttpServletRequest request, HttpServletResponse response, int dip_key, Date dataI,
+      Date dataF)
+      throws IOException, ServletException, TemplateManagerException {
+
+    StringBuffer url = new StringBuffer();
+
+    url.append("EsportaEventiVSC?");
+    url.append("dip_key=" + dip_key);
+    url.append("&&");
+    url.append("dateI=" + dataI);
+    url.append("&&");
+    url.append("dateF=" + dataF);
+
+    response.sendRedirect(url.toString());
+  }
+
   @Override
   protected void processRequest(HttpServletRequest request, HttpServletResponse response)
       throws ServletException {
@@ -140,6 +155,10 @@ public class SceltaCriterio extends AuleWebBaseController {
         } else if (request.getParameter("action").equals("confGiorno")) {
           Date data = Date.valueOf(request.getParameter("data"));
           action_giorno(request, response, dip_key, data);
+        } else if (request.getParameter("action").equals("confIntervallo")) {
+          Date dataI = Date.valueOf(request.getParameter("dataI"));
+          Date dataF = Date.valueOf(request.getParameter("dataF"));
+          action_esporta(request, response, dip_key, dataI, dataF);
         }
       }
 
